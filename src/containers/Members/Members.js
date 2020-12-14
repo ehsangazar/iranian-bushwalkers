d3659830dd074b714f13fb67bcb62bf512a735e0
Ehsan Gazar <me@ehsangazar.com>
2020-12-15 07:44:41 +1100

    added a search

--- src/containers/Members/Members.js
+++ src/containers/Members/Members.js
@@ -4,7 +4,7 @@ import SinglePostSection from '../SinglePostSection/SinglePostSection'
 import ContentMiddleBackgroundSolid from '../ContentMiddleBackgroundSolid/ContentMiddleBackgroundSolid'
 import hamid from "../BoardMembers/hamid.jpg"
 import ehsan from "../BoardMembers/ehsan.png"
-import { Container, Row , Col, Table } from 'react-bootstrap'
+import { Container, Row , Table, Form } from 'react-bootstrap'
 import Space from '../Space/Space'
 import { Link } from 'react-router-dom'
 import fetchHandler from '../../utils/fetchHandler'
@@ -12,6 +12,8 @@ import LoadingPage from '../LoadingPage/LoadingPage'
 
 const Members = () => {
   const [users, setUsers] = useState([])
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [search, setSearch] = useState(null)
 
   const getUsers = async () => {
     try {
@@ -21,13 +23,33 @@ const Members = () => {
       })
       if (result?.data?.success) {
         setUsers(result.data.users)
+        setFilteredUsers(result.data.users)
       } else {
         setUsers([])
+        setFilteredUsers([])
       }
     } catch (e) {
       console.error(e)
     }
   }
+
+  const handleSearch = (event) => {
+    if (event) event.preventDefault()
+    setSearch(event.target.value)
+  }
+
+  useEffect(()=>{
+    if (!search || search === '' || search === null){
+      setFilteredUsers(users)
+    } else {
+      const newUsers = users.filter((user) => {
+        if (user.first_name.toLowerCase().includes(search.toLowerCase())) return true
+        if (user.last_name.toLowerCase().includes(search.toLowerCase())) return true
+        return false
+      })
+      setFilteredUsers(newUsers)
+    }
+  },[search])
 
   useEffect(()=>{
     getUsers()
@@ -45,10 +67,30 @@ const Members = () => {
         <ContentMiddleBackgroundSolid title="Members" />
         <Space />
         <Container>
+          <Row style={{
+              padding: '20px', 
+              paddingBottom: 0,
+              width: '100%',
+              margin: 0,
+            }}
+          >
+            <Form.Group  style={{
+              width: '100%'
+            }}
+            >
+              <h3>Search</h3>
+              <Form.Control
+                onChange={handleSearch}
+                type="text"
+                value={search}
+                placeholder="Search here"                
+              />
+            </Form.Group>
+          </Row>
           <Row style={{padding: '20px'}}>
             <Table striped bordered hover>
               <tbody>
-                {users.map(user => (
+                {filteredUsers.map(user => (
                   <tr>
                     <td><img style={{width: '100%', maxWidth: '100px', paddingRight: '10px'}} src={user.image} alt={`${user.first_name} ${user.last_name}`} /></td>
                     <td>{`${user.first_name} ${user.last_name}`}</td>
