4c98b53bae618b16113ddbd6b2620959f46362ce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-05 22:20:13 +0000

    Deploy app.ib.org.au to github.com/ehsangazar/iranian-bushwalkers.git:gh-pages

---
+++ precache-manifest.6b5d33a2788b6ecaed0c777140b2d00a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e389d0a92ca15e0b4aa4613374801392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de065c3e367f041ff5e3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "878c27055ae0d45f8939",
+    "url": "/static/css/main.87c292d4.chunk.css"
+  },
+  {
+    "revision": "de065c3e367f041ff5e3",
+    "url": "/static/js/2.af342568.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af342568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878c27055ae0d45f8939",
+    "url": "/static/js/main.80f4dae5.chunk.js"
+  },
+  {
+    "revision": "86ff8a3d9b6b3fce872a",
+    "url": "/static/js/runtime-main.1c735113.js"
+  },
+  {
+    "revision": "85ba71e4cafb8edb32875142e12ae6dd",
+    "url": "/static/media/about.85ba71e4.jpg"
+  },
+  {
+    "revision": "d506a1d71d3f4b9ea5faeb7d8c816427",
+    "url": "/static/media/amir.d506a1d7.png"
+  },
+  {
+    "revision": "ba4ee10156c8a8c55c43cc769fb8d1fc",
+    "url": "/static/media/banner.ba4ee101.jpg"
+  },
+  {
+    "revision": "15163083f4957dd7e3ea5a15ce1f9aa7",
+    "url": "/static/media/borzu.15163083.png"
+  },
+  {
+    "revision": "955745b030c97562c32f07cf3c662baf",
+    "url": "/static/media/ehsan.955745b0.png"
+  },
+  {
+    "revision": "a9ed812e48aeac69b88a2f92973b018f",
+    "url": "/static/media/golsa.a9ed812e.png"
+  },
+  {
+    "revision": "0fec5d3938d2d797cce02f5b001fbfa3",
+    "url": "/static/media/grades.0fec5d39.jpg"
+  },
+  {
+    "revision": "4f2cd0941f5e6ca5428a07fe39687154",
+    "url": "/static/media/hamed.4f2cd094.png"
+  },
+  {
+    "revision": "34fa8489ca9dfe7e75e4251116dcbadd",
+    "url": "/static/media/hamid.34fa8489.jpg"
+  },
+  {
+    "revision": "0605c459d7324a4146a8b57052612ea7",
+    "url": "/static/media/hoda.0605c459.png"
+  },
+  {
+    "revision": "415903382b7b43060b45ae06224dc9a1",
+    "url": "/static/media/kamran.41590338.png"
+  },
+  {
+    "revision": "8f0c15dd7a9d55060e67838a92ecc3ad",
+    "url": "/static/media/kazem.8f0c15dd.png"
+  },
+  {
+    "revision": "2e2be0fb0697694189797cf79fa4f47f",
+    "url": "/static/media/loading.2e2be0fb.gif"
+  },
+  {
+    "revision": "9c80a1d5bbe3317605228280a43b2bfb",
+    "url": "/static/media/logo.9c80a1d5.png"
+  },
+  {
+    "revision": "9450746b924a9b9f9ff9371a8d51fa9e",
+    "url": "/static/media/mehdi.9450746b.png"
+  },
+  {
+    "revision": "dacd6f2440dd1ef134914e7d028ca5b9",
+    "url": "/static/media/negar.dacd6f24.png"
+  },
+  {
+    "revision": "ee871571c87d7d1591a9a4cd9a21744d",
+    "url": "/static/media/tannaz.ee871571.png"
+  },
+  {
+    "revision": "e31d35ae9891dff6a32cf38955c107ae",
+    "url": "/static/media/unseen.e31d35ae.png"
+  }
+]);
